0c15b4e1e53ccd6e331e396fa596a33bd2a2b56c
m1tch191 <87219656+m1tch191@users.noreply.github.com>
2021-12-09 19:43:22 -0500

    Update RocketController.cs

--- Invaders/Assets/Scripts/RocketController.cs
+++ Invaders/Assets/Scripts/RocketController.cs
@@ -6,11 +6,15 @@ public class RocketController : MonoBehaviour
 {
     [Header("Variables")]
     public float thrust;
-    public float controlModifier = 1f;
+    public float controlModifier = 1f; //This number is mulitplied by thrust to give a lower thrust to the control thrusters
 
     private Vector3 moveDirection = Vector3.zero;
 
     [Header("Particle System")]
+    // Setting up the process of being able to enable and disable the particle effect for control thrusters
+    // Not working as intended, if multiple inputs are pressed all emissions stay on until nothing is pressed
+
+    // Also there has to be a better way of setting this up because there is no way i need 3 varibles just to get access to ParticleSystem.Emission
     public GameObject verticalThrusterPS;
     public GameObject forwardThrusterPS;
     public GameObject reverseThrusterPS;
@@ -23,92 +27,73 @@ public class RocketController : MonoBehaviour
     ParticleSystem leftps;
     ParticleSystem rightps;
 
+    ParticleSystem.EmissionModule emissionVertical;
+    ParticleSystem.EmissionModule emissionForward;
+    ParticleSystem.EmissionModule emissionReverse;
+    ParticleSystem.EmissionModule emissionLeft;
+    ParticleSystem.EmissionModule emissionRight;
+
     //Other variables
-    Rigidbody rb;
+    Rigidbody rb; 
 
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        vertps = verticalThrusterPS.GetComponent<ParticleSystem>();
+        rb = GetComponent<Rigidbody>();     //Getting rigidbody so force can be added
+
+        // Same with here there has got to be a better way of setting this up
+
+        vertps = verticalThrusterPS.GetComponent<ParticleSystem>();     // Just grabbing the component off of the object, typical unity stuff
         forps = forwardThrusterPS.GetComponent<ParticleSystem>();
         revps = reverseThrusterPS.GetComponent<ParticleSystem>();
         leftps = leftThrusterPS.GetComponent<ParticleSystem>();
         rightps = rightThrusterPS.GetComponent<ParticleSystem>();
+
+        emissionVertical = vertps.emission;
+        emissionForward = forps.emission;
+        emissionReverse = revps.emission;
+        emissionLeft = leftps.emission;
+        emissionRight = rightps.emission;
     }
 
     private void FixedUpdate()
     {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")); // Getting user input and storing into Vector (Perhaps switch this to proper keybinds and that
+                                                                                                 //                                             would relieve issues with Particle System)
         if(moveDirection.z > 0.1f)
         {
-            forps.enableEmission = true;
-            rb.AddRelativeForce(new Vector3(-thrust * controlModifier, 0f, 0f));
+            emissionForward.enabled = true;                                         // Emission on Particle System is enabled
+            rb.AddRelativeForce(new Vector3(-thrust * controlModifier, 0f, 0f));    // Corresponding force is applied in the direction the control thruster is pushing
         }
         if (moveDirection.z < -0.1f)
         {
-            revps.enableEmission = true;
+            emissionReverse.enabled = true;
             rb.AddRelativeForce(new Vector3(thrust * controlModifier, 0f, 0f));
         }
         if (moveDirection.x > 0.1f)
         {
-            leftps.enableEmission = true;
+            emissionLeft.enabled = true;
             rb.AddRelativeForce(new Vector3(0f, 0f, thrust * controlModifier));
         }
         if (moveDirection.x < -0.1f)
         {
-            rightps.enableEmission = true;
+            emissionRight.enabled = true;
             rb.AddRelativeForce(new Vector3(0f, 0f, -thrust * controlModifier));
         }
-        else {
-            forps.enableEmission = false;
-            revps.enableEmission = false;
-            leftps.enableEmission = false;
-            rightps.enableEmission = false;
-        }
-
         if (Input.GetButton("Jump"))
         {
-            vertps.enableEmission = true;
+            emissionVertical.enabled = true;
             rb.AddRelativeForce(new Vector3(0f, thrust, 0f));
         }
         else
         {
-            vertps.enableEmission = false;
-            
+            emissionForward.enabled = false;    // Disable all Particle Systems if nothing is pressed
+            emissionReverse.enabled = false;
+            emissionLeft.enabled = false;
+            emissionRight.enabled = false;
+            emissionVertical.enabled = false;
         }
+
+       
     }
 }
-
-
-
-
-//public float thrust;
-//public GameObject pRocket;
-//ParticleSystem ps;
-
-//Rigidbody rb;
-
-//void Start()
-//{
-//    rb = GetComponent<Rigidbody>();
-//    ps = pRocket.GetComponent<ParticleSystem>();
-
-//}
-
-//// Update is called once per frame
-//void FixedUpdate()
-//{
-//    var emisRocket = ps.emission;
-
-//    if (Input.GetButton("Jump"))
-//    {
-//        ps.Play();
-//        rb.AddRelativeForce(new Vector3(0f, thrust, 0f));
-//    }
-//    else
-//    {
-//        ps.Stop();
-//    }
-//}
